45ba10bdfeaa1dc734c78a02b0408bfcaea0674f
Marko Tkalcic <tkalcic.marko@gmail.com>
2018-09-04 19:30:41 +0530

    made it more simple

--- ASCII_art_exercise.py
+++ ASCII_art_exercise.py
@@ -2,29 +2,17 @@ from pyfiglet import figlet_format
 from termcolor import colored
 
 
+valid_colors = ( "red", "green", "yellow", "blue", "magenta", "cyan", "white")
 
-while True:
-	user_messege = input("What massage you wanna print?: ")
-	user_color = input("What color?: ")
-	user_messege_text = figlet_format(user_messege, font='standard')
-	if user_color == "red":
-		print(colored(user_messege_text, color = "red", attrs = ["blink"]))
-		break
-	elif user_color == "blue":
-		print(colored(user_messege_text, color = "blue", attrs = ["blink"]))
-		break
-	elif user_color == "magenta":
-		print(colored(user_messege_text, color = "magenta", attrs = ["blink"]))
-		break
-	elif user_color == "cyan":
-		print(colored(user_messege_text, color = "cyan", attrs = ["blink"]))
-		break
-	elif user_color == "yellow":
-		print(colored(user_messege_text, color = "yellow", attrs = ["blink"]))
-		break
-	else:
-		print("Try Again")
-	
+user_messege = input("What massage you wanna print?: ")
+user_color = input("What color?: ")
+
+if user_color not in valid_colors:
+	user_color = "cyan"
+
+user_messege_text = figlet_format(user_messege)
+print(colored(user_messege_text, color = user_color, attrs = ["blink"]))
+
 
 
 
